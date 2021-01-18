62b86204a1752c98fec351c0f91ff8e09edffd6c
Luq7man <luqman@joudy.com>
2021-01-18 21:48:05 +0200

    Enable button logic

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ let headline = document.getElementsByTagName("h1")[0];
 headline.onclick = function() {
 	alert("you clicked on page headline");
 }
+
+function inputFeature() {
+	let input = document.getElementsByTagName("input")[0];
+	let printer = document.getElementById("printer");
+	function changeHeadline(newText) {
+		headline.textContent = newText;
+	}
+	
+	function getInputText() {
+		return input.value;
+	}
+	
+	function addClickEvent() {
+		printer.onclick = function() {
+			let textToAdd = getInputText();
+			changeHeadline(textToAdd);
+		}
+	}
+	return {
+		on: function() {
+			addClickEvent();
+		}
+	}
+}
+
+inputFeature().on();
