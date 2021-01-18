a8238c6b9128c04118c56ceef04f9e063d6d335c
Luq7man <luqman@joudy.com>
2021-01-18 21:37:15 +0200

    Enabled button logic

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ let headline = document.getElementsByTagName("h1")[0];
 headline.onclick = function() {
 	alert("you clicked on page headline");
 }
+
+function paragraphFeature() {
+	let changed = false;
+	let par = document.getElementsByTagName("p")[0];
+	let btn = document.getElementById("changer");
+	function changeColor() {
+		par.style.color = "red";
+	}
+	function addEvent() {
+		btn.onclick = function () {
+			if(! changed)
+				changeColor();
+		}
+	}
+	return {
+		on: function() {
+			addEvent();
+		}
+	}
+}
+
+paragraphFeature().on();
